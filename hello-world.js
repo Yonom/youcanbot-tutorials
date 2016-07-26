86b76cd3252995718ea112f2a761e657477b66c5
Sepehr Farshid / Processor <sepehr1376@gmail.com>
2016-07-26 18:31:17 +0200

    Add a hello world tutorial

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+var toolbox = '<xml>';
+toolbox += '<block type="text"></block>';
+toolbox += '<block type="text_chat"></block>';
+toolbox += '</xml>';
+
+initTutorial({
+    title: 'Hello world!',
+    objective: 'Objective: Make the bot say "Hello world" in chat.\n\nOnce you are ready, press the <b>Play</b> button above to start.',
+    toolbox: toolbox
+});
