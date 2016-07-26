82c6ae72ce3ce67c10b0d18c466f582df14bead2
Sepehr Farshid / Processor <sepehr1376@gmail.com>
2016-07-26 20:21:38 +0200

    Add sandbox

--- hello-world.js
+++ hello-world.js
@@ -1,10 +1,10 @@
 var toolbox = '<xml>';
+toolbox += '<block type="actions_chat"></block>';
 toolbox += '<block type="text"></block>';
-toolbox += '<block type="text_chat"></block>';
 toolbox += '</xml>';
 
 initTutorial({
     title: 'Hello world!',
-    objective: 'Objective: Make the bot say "Hello world" in chat.<br><br>Once you are ready, press the <b>Play</b> button above to start.',
+    objective: 'Objective: Make the bot say "Hello world" in chat.<br><br>Once you are ready, press the <b>PLAY</b> button above to run your program.',
     toolbox: toolbox
 });
