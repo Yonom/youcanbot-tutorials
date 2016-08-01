9c8f52bbe5569129da505c13a8abb78b26eb0c65
Sepehr Farshid / Processor <sepehr1376@gmail.com>
2016-08-01 22:52:23 +0200

    Use toolbox for hello-world

--- tutorials/hello-world.js
+++ tutorials/hello-world.js
@@ -1,6 +1,17 @@
 var toolbox = '<xml>';
-toolbox += '    <block type="actions_chat"></block>';
-toolbox += '    <block type="text"></block>';
+toolbox += '    <category name="Text" colour="160">';
+toolbox += '        <block type="text"></block>';
+toolbox += '    </category>';
+toolbox += '    <sep></sep>';
+toolbox += '    <category name="Actions" colour="20">';
+toolbox += '        <block type="actions_chat">';
+toolbox += '            <value name="TEXT">';
+toolbox += '                <shadow type="text">';
+toolbox += '                    <field name="TEXT">abc</field>';
+toolbox += '                </shadow>';
+toolbox += '            </value>';
+toolbox += '        </block>';
+toolbox += '    </category>';
 toolbox += '</xml>';
 
 initTutorial({
